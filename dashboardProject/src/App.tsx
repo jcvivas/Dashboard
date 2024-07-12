17c04e820195ec1cd27b44fae48a9e6da3902b31
jcvivas <jcvivas@espol.edu.ec>
2024-07-12 16:51:16 -0500

    prueba2

--- dashboardProject/src/App.tsx
+++ dashboardProject/src/App.tsx
@@ -2,8 +2,8 @@
 import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
 
 // import Indicator from "./components/Indicator";
-// import Summary from "./components/Summary";
-// //import BasicTable from "./components/BasicTable";
+import Summary from "./components/Summary";
+import BasicTable from "./components/BasicTable";
 // import WeatherChart from "./components/WeatherChart";
 // import ControlPanel from "./components/ControlPanel";
 
@@ -11,14 +11,20 @@ import "./App.css";
 
 function App() {
   return (
-    <Grid>
-        <Grid>1</Grid>
-        <Grid>2</Grid>
-        <Grid>3</Grid>
-        <Grid>4</Grid>
-        <Grid>5</Grid>
-        <Grid>6</Grid>
-      </Grid>
+    <Grid container spacing={5}>
+        <Grid xs={12} sm={4} md={3} lg={2}>1</Grid>
+        <Grid xs={6} sm={4} md={3} lg={2}>2</Grid>
+        <Grid xs={6} sm={4} md={3} lg={2}>3</Grid>
+        <Grid xs={12} sm={4} md={3} lg={2}>4</Grid>
+        <Grid xs={6} sm={4} md={6} lg={2}>5</Grid>
+        <Grid xs={6} sm={4} md={6} lg={2}>6</Grid>
+        <Grid xs={6} sm={4} md={3} lg={2}>
+        <Grid xs={12} md={6} lg={9} >
+ 	       <BasicTable />
+ 	      </Grid>    
+ 	      <Summary></Summary></Grid>
+        </Grid>
+        
   )
 }
 
