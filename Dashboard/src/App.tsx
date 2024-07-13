2a3f018e3a309f78899d58d26856ab71bd761d6f
jcvivas <jcvivas@espol.edu.ec>
2024-07-13 01:16:33 -0500

    reinicio por perdida de archivos

--- Dashboard/src/App.tsx
+++ Dashboard/src/App.tsx
@@ -1,188 +1,143 @@
-import { useEffect, useState } from "react";
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import Grid from "@mui/material/Unstable_Grid2";
-import Indicator from "./components/Indicator";
-import Summary from "./components/Summary";
-import BasicTable from "./components/BasicTable";
-import WeatherChart from "./components/WeatherChart";
-import ControlPanel from "./components/ControlPanel";
+// import Indicator from "./components/Indicator";
+// import Summary from "./components/Summary";
+// import BasicTable from "./components/BasicTable";
+// import WeatherChart from "./components/WeatherChart";
+// import ControlPanel from "./components/ControlPanel";
+
 import "./App.css";
+import {  useState } from "react";
 
 function App() {
   {
     /* Variable de estado y función de actualización */
   }
 
-  let [indicators, setIndicators] = useState([]);
-  let [rowsTable, setRowsTable] = useState([]);
-  {
-    /* Función para el efecto secundario a ejecutar y Arreglo de dependencias */
-  }
+  // const [indicators, setIndicators] = useState([])
+  // const [rowsTable, setRowsTable] = useState([])
+  // {
+  //   /* Función para el efecto secundario a ejecutar y Arreglo de dependencias */
+  // }
 
-  useEffect(() => {
-    (async () => {
-      {
-        /* Request */
-      }
+  // useEffect(() => {
+  //   (async () => {
 
-      // let API_KEY = "81aebda6dbd5149683c967788c6a0b21";
-      // let response = await fetch(
-      //   `https://api.openweathermap.org/data/2.5/forecast?q=Guayaquil&mode=xml&appid=${API_KEY}`
-      // );
+  //     let savedTextXML = localStorage.getItem("openWeatherMap");
+  //     let expiringTime = localStorage.getItem("expiringTime");
 
-      {
-        /* 2. Del LocalStorage, obtiene el valor de las claves openWeatherMap y expiringTime */
-      }
+  //     {
+  //       /* 3. Obtenga la estampa de tiempo actual */
+  //     }
 
-      let savedTextXML = localStorage.getItem("openWeatherMap");
-      let expiringTime = localStorage.getItem("expiringTime");
+  //     let nowTime = new Date().getTime();
+  //     {
+  //       /* 4. Realiza la petición asicrónica cuando: 
+  //                (1) La estampa de tiempo de expiración (expiringTime) es nula, o  
+  //                (2) La estampa de tiempo actual es mayor al tiempo de expiración */
+  //     }
 
-      {
-        /* 3. Obtenga la estampa de tiempo actual */
-      }
+  //     if (expiringTime === null || nowTime > parseInt(expiringTime)) {
+  //       {
+  //         /* 5. Request */
+  //       }
 
-      let nowTime = new Date().getTime();
-      {
-        /* 4. Realiza la petición asicrónica cuando: 
-                 (1) La estampa de tiempo de expiración (expiringTime) es nula, o  
-                 (2) La estampa de tiempo actual es mayor al tiempo de expiración */
-      }
+  //       let API_KEY = "81aebda6dbd5149683c967788c6a0b21";
+  //       let response = await fetch(
+  //         `https://api.openweathermap.org/data/2.5/forecast?q=Guayaquil&mode=xml&appid=${API_KEY}`
+  //       );
+  //       savedTextXML = await response.text();
 
-      if (expiringTime === null || nowTime > parseInt(expiringTime)) {
-        {
-          /* 5. Request */
-        }
+  //       {
+  //         /* 6. Diferencia de tiempo */
+  //       }
 
-        let API_KEY = "81aebda6dbd5149683c967788c6a0b21";
-        let response = await fetch(
-          `https://api.openweathermap.org/data/2.5/forecast?q=Guayaquil&mode=xml&appid=${API_KEY}`
-        );
-        savedTextXML = await response.text();
+  //       let hours = 1;
+  //       let delay = hours * 3600000;
 
-        {
-          /* 6. Diferencia de tiempo */
-        }
+  //       {
+  //         /* 7. En el LocalStorage, almacena texto en la clave openWeatherMap y la estampa de tiempo de expiración */
+  //       }
 
-        let hours = 1;
-        let delay = hours * 3600000;
+  //       localStorage.setItem("openWeatherMap", savedTextXML);
+  //       localStorage.setItem("expiringTime", (nowTime + delay).toString());
+  //     }
 
-        {
-          /* 7. En el LocalStorage, almacena texto en la clave openWeatherMap y la estampa de tiempo de expiración */
-        }
+  //     {
+  //       /* XML Parser */
+  //     }
+      
+  //     const parser = new DOMParser();
+  //     const xml = parser.parseFromString(savedTextXML, "application/xml");
 
-        localStorage.setItem("openWeatherMap", savedTextXML);
-        localStorage.setItem("expiringTime", (nowTime + delay).toString());
-      }
 
-      {
-        /* XML Parser */
-      }
-      const parser = new DOMParser();
-      const xml = parser.parseFromString(savedTextXML, "application/xml");
+  //     {
+  //       /* Arreglo para agregar los resultados */
+  //     }
 
-      {
-        /* Arreglo para agregar los resultados */
-      }
+  //     let dataToIndicators = new Array();
 
-      let dataToIndicators = new Array();
+  //     {
+  //       /* 
+  //         Análisis, extracción y almacenamiento del contenido del XML 
+  //         en el arreglo de resultados
+  //     */
+  //     }
 
-      {
-        /* 
-          Análisis, extracción y almacenamiento del contenido del XML 
-          en el arreglo de resultados
-      */
-      }
+  //     let location = xml.getElementsByTagName("location")[1];
 
-      let location = xml.getElementsByTagName("location")[1];
+  //     let geobaseid = location.getAttribute("geobaseid");
+  //     dataToIndicators.push(["Location", "geobaseid", geobaseid]);
 
-      let geobaseid = location.getAttribute("geobaseid");
-      dataToIndicators.push(["Location", "geobaseid", geobaseid]);
+  //     let latitude = location.getAttribute("latitude");
+  //     dataToIndicators.push(["Location", "Latitude", latitude]);
 
-      let latitude = location.getAttribute("latitude");
-      dataToIndicators.push(["Location", "Latitude", latitude]);
-
-      let longitude = location.getAttribute("longitude");
-      dataToIndicators.push(["Location", "Longitude", longitude]);
+  //     let longitude = location.getAttribute("longitude");
+  //     dataToIndicators.push(["Location", "Longitude", longitude]);
       
 
-      //console.log( dataToIndicators )
+  //     //console.log( dataToIndicators )
 
-      let indicatorsElements = Array.from(dataToIndicators).map((element) => (
-        <Indicator
-          title={element[0]}
-          subtitle={element[1]}
-          value={element[2]}
-        />
-      ));
+  //     let indicatorsElements = Array.from(dataToIndicators).map((element) => (
+  //       <Indicator
+  //         title={element[0]}
+  //         subtitle={element[1]}
+  //         value={element[2]}
+  //       />
+  //     ));
 
-      {
-        /* Modificación de la variable de estado mediante la función de actualización */
-      }
+  //     {
+  //       /* Modificación de la variable de estado mediante la función de actualización */
+  //     }
 
-      setIndicators(indicatorsElements);
-       {/* 
-                 2. Procese los resultados de acuerdo con el diseño anterior.
-                    Revise la estructura del documento XML para extraer los datos necesarios. 
-             */}
+  //     setIndicators(indicatorsElements);
+  //      {/* 
+  //                2. Procese los resultados de acuerdo con el diseño anterior.
+  //                   Revise la estructura del documento XML para extraer los datos necesarios. 
+  //            */}
              
 
-             let arrayObjects = Array.from( xml.getElementsByTagName("time") ).map( (timeElement) =>  {
+  //            let arrayObjects = Array.from( xml.getElementsByTagName("time") ).map( (timeElement) =>  {
 					
-              let rangeHours = timeElement.getAttribute("from").split("T")[1] + " - " + timeElement.getAttribute("to").split("T")[1]
+  //             let rangeHours = timeElement.getAttribute("from").split("T")[1] + " - " + timeElement.getAttribute("to").split("T")[1]
 
-              let windDirection = timeElement.getElementsByTagName("windDirection")[0].getAttribute("deg") + " "+  timeElement.getElementsByTagName("windDirection")[0].getAttribute("code") 
+  //             let windDirection = timeElement.getElementsByTagName("windDirection")[0].getAttribute("deg") + " "+  timeElement.getElementsByTagName("windDirection")[0].getAttribute("code") 
        
-              return { "rangeHours": rangeHours,"windDirection": windDirection }
+  //             return { "rangeHours": rangeHours,"windDirection": windDirection }
      
-          })
-          arrayObjects = arrayObjects.slice(0,8)
+  //         })
+  //         arrayObjects = arrayObjects.slice(0,8)
 			
-             {/* 3. Actualice de la variable de estado mediante la función de actualización */}
+  //            {/* 3. Actualice de la variable de estado mediante la función de actualización */}
 
-             setRowsTable(arrayObjects)
-    })();
-  }, []);
+  //            setRowsTable(arrayObjects)
+  //   })();
+  // }, []);
 
   return (
     <Grid container spacing={5}>
-      <Grid xs={12} lg={12}>
-        <p>Dashboard</p>
-      </Grid>
-      <Grid xs={6} lg={2}>
-        {indicators[0]}
-
-        {/* <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} /> */}
-      </Grid>
-
-      <Grid xs={6} lg={2}>
-        {indicators[1]}
-
-        {/* <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} /> */}
-      </Grid>
-
-      <Grid xs={6} lg={2}>
-        {indicators[2]}
-
-        {/* <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} /> */}
-      </Grid>
-      
-      <Grid xs={6} md={4} lg={2}>
-        <Indicator title="Precipitación" subtitle="Probabilidad" value={0.13} />
-      </Grid>
-      <Grid xs={6} sm={4} md={3} lg={2}>
-        <Summary></Summary>
-      </Grid>
-      <Grid xs={12} md={6} lg={9}>
-        <BasicTable rows={rowsTable}></BasicTable>
-      </Grid>
-      <Grid xs={12} lg={2}>
-        <ControlPanel />
-      </Grid>
-      <Grid xs={12} lg={10}>
-
-        <WeatherChart></WeatherChart>
-      </Grid>
+      <p>Hola</p>
     </Grid>
   );
 }
